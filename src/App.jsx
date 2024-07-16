b07f41058efe7c3fac4de5b3765c0a08165c8a8e
Mohit <mohitkhandelwal579@gmail.com>
2024-07-16 19:21:58 +0530

    added all the basic to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './Pages/HomePage'
 
 function App() {
 
   return (
-    <>
-    lms
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage/>} ></Route>
+    </Routes>
   )
 }
 
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,17 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState={
-    isLoggedIn:  localStorage.get('isLoggedIn') || false,
-    role:localStorage.get('role') || "",
-    data:localStorage.get('data') || {},
+const getLocalStorageItem = (key, defaultValue) => {
+    if (typeof window !== "undefined" && window.localStorage) {
+        return localStorage.getItem(key) || defaultValue;
+    }
+    return defaultValue;
 };
 
-const authSlice=createSlice({
-    name:'auth',
+const initialState = {
+    isLoggedIn: getLocalStorageItem('isLoggedIn', false),
+    role: getLocalStorageItem('role', ""),
+    data: JSON.parse(getLocalStorageItem('data', '{}')),
+};
+
+const authSlice = createSlice({
+    name: 'auth',
     initialState,
-    reducers:{},
+    reducers: {},
 });
 
-export const {} =authSlice.actions;
+export const {} = authSlice.actions;
 export default authSlice.reducer;
